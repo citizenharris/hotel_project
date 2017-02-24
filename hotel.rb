8ba79b32730d8617c6313cecc2e765e7b301db4a
George Harris <citizenharris@gmail.com>
2017-02-24 10:37:44 +0000

    Passed tests

---
+++ hotel.rb
@@ -0,0 +1,30 @@
+class Hotel
+  attr_accessor :name, :rating, :city, :total_rooms, :features
+  
+  @@hotels = []
+  def initialize(name:, rating:, city:, total_rooms:, features:)
+    @name = name
+    @rating = rating
+    @city = city
+    @total_rooms = total_rooms
+    @features = features
+    @@hotels << self
+  end
+
+
+  def self.all
+    @@hotels
+  end
+
+  def self.last
+    @@hotels[-1]
+  end
+
+  def stars
+    return "⭐️"* @rating
+  end
+
+  def slug
+    return @name.gsub(/[ ]/, "_").downcase
+  end
+end
